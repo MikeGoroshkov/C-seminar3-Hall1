0cdbc53d1c22f98a2c5fd2a63352d410fa2938b8
Mikhail Goroshkov <gormike86@gmail.com>
2022-10-07 21:45:33 +0300

    seminar 3

---
+++ 19/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет 23432 -> да 12821 -> да
+Console.Write("Введите пятизначное число n: ");
+int n = int.Parse(Console.ReadLine());
+int n1 = n/10000;
+int n2 = (n/1000)%10;
+int n4 = (n/10)%10;
+int n5 = n%10;
+Console.Write(n1);
+Console.Write(n2);
+Console.Write(n4);
+Console.Write(n5);
+if (n1 == n5 && n2 == n4) {Console.WriteLine($"Число {n} является палиндромом");}
+else {Console.WriteLine($"Число {n} не является палиндромом");}
---
+++ 3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух 
+//точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21 
+Console.Write("Введите X первой точки: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите X второй точки: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+int y2 = int.Parse(Console.ReadLine());
+int d = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+double c = Math.Sqrt(d);
+Console.WriteLine(c);
